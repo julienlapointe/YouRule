b4ccb7bbecb727600cc42b136529acb980ff64a4
Julien LaPointe <email@julienlapointe.com>
2020-12-06 13:28:47 -0500

    Selected all text elements and added to array if not empty.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,24 @@ window.onload = function highlightLineOnHover() {
 
 	// var paragraphs = document.getElementsByTagName('div');
 
+	var array = [];
+	// var foundFirstHeading = false;
+	// var foundLastParagraph = false;
+
+    var elements = document.body.querySelectorAll('h1, h2, h3, h4, h5, h6, p, pre, span, article, section, blockquote, ol, ul, li, strong, b, em')
+
+	// https://stackoverflow.com/questions/17727977/how-to-get-all-text-from-all-tags-in-one-array
+    for (var i = 0; i < elements.length; i++) {
+       var current = elements[i];
+	   // Check the element has no children and that it is not empty
+        if (current.children.length === 0 && current.textContent.replace(/ |\n/g,'') !== '') {
+			var element = [];
+			element.push(current.nodeName);
+			element.push(current.textContent);
+			array.push(element);
+        }
+    }
+
 	// Splits text into lines, words, characters (default)
 	// const splitType = new SplitType('h1, h2, h3, h4, h5, h6, p, pre, span, article, section, blockquote, ol, ul, li, strong, b, em', { types: 'lines', lineClass: 'highlightLineOnHover' } );
 	const splitType = new SplitType('.overlayContent', { types: 'lines', lineClass: 'highlightLineOnHover' } );
